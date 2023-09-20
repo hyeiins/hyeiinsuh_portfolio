531c1adc17cb6cc27c6e0587c56a55a76c597dbe
hyeiins <hyeiinsuh92@gmail.com>
2023-09-19 23:09:31 -0400

    test auto deployment

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Hyei-In's portfolio" />
+      <HelloWorld msg="This is Hyei-In's portfolio" />
     </div>
   </header>
 
